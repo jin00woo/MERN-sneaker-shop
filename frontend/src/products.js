6c727b64e0affaf573f7ee6ebcf8635de8e745ab
Jin Woo Han <jin00woo@naver.com>
2023-08-16 16:01:42 +0900

    Added dummy products.

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Air Jordan 1 Retro High 'Bred'",
+    image: "/images/air_jordan_1_bred.jpeg",
+    description:
+      "The Air Jordan 1 Retro High OG 'Bred' 2016 brings back the colorway that started it all. The high-top features a black and red upper constructed with a mix of leather and OG-style satin. A white midsole and red outsole complete the look.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 400,
+    countInStock: 10,
+    rating: 4.8,
+    numReviews: 10,
+  },
+  {
+    _id: "2",
+    name: "Air Jordan 3 Retro 'Cement'",
+    image: "/images/air_jordan_3_cement.jpeg",
+    description:
+      "The Air Jordan 3 Retro 'Cement' 2011 brings back the iconic white and grey colorway to the Jordan 3 silhouette. The shoe features a white leather upper with grey elephant print overlays and black accents on the tongue, heel and midsole. The Air Jordan 3 Retro 'Cement' 2011 also features the Jumpman logo on the tongue and heel tab.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 350,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Air Jordan 6 Retro 'Infrared'",
+    image: "/images/air_jordan_6_infrared.jpeg",
+    description:
+      "The Air Jordan 6 Retro 'Infrared' 2019 brings back the classic colorway of the iconic silhouette. The shoe's black nubuck upper includes a reflective 3M layer with perforations throughout for ventilation. The heel, tongue and lock sport Infrared accents, with the color also featured on the signature midsole detailing. The shoe's black and Infrared colorway was made famous in 1991 when Michael Jordan wore it during the course of his first NBA championship.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 300,
+    countInStock: 5,
+    rating: 4.0,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Air Max 90 'Blue Chill'",
+    image: "/images/air_max_90_bluechill.jpeg",
+    description:
+      "The Air Max 90 'Blue Chill' features a colorway inspired by the OG 'Blue Chill' Air Max 90. The shoe's upper is built with a mix of mesh and leather, finished in a mix of white and skyblue. The signature Max Air unit in the heel provides cushioning, giving way to a rubber outsole for traction.",
+    brand: "Nike",
+    category: "Sneakers",
+    price: 150,
+    countInStock: 11,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Yeezy Boost 700 'Analog'",
+    image: "/images/yeezy_boost_analog.jpeg",
+    description:
+      "The adidas Yeezy Boost 700 'Analog' is a study in tonal minimalism, highlighted by a neutral color scheme that can be seamlessly integrated into any sneaker rotation. The shoe's upper is constructed from a blend of mesh and suede, finished in varying shades of grey. The monochromatic build is contrasted by a cream-colored full-length drop-in Boost midsole, supported underfoot by a black rubber outsole.",
+    brand: "Adidas",
+    category: "Sneakers",
+    price: 300,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "New Balance 990 'Black'",
+    image: "/images/new_balance_990.jpeg",
+    description:
+      "The New Balance 990 'Black' features a stealthy colorway of the classic silhouette. The shoe's upper is built with a mix of mesh and pigskin suede, finished in black and accented by reflective detailing on the heel and 'N' logo. Underfoot, the ENCAP midsole provides cushioning, giving way to a black rubber outsole for traction.",
+    brand: "New Balance",
+    category: "Sneakers",
+    price: 175,
+    countInStock: 3,
+    rating: 4.9,
+    numReviews: 12,
+  },
+];
+
+export default products;

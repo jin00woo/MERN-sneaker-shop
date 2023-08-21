7049aad9cecafaf35b620c4da97d1c77c22d553b
Jin Woo Han <jin00woo@naver.com>
2023-08-21 16:03:18 +0900

    Started working on backend.

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import products from "./data/products.js";
+const port = 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((x) => x._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});

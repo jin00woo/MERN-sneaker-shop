a946ce15627b44edd46900d672b4cacd4b6b1bd8
Jin Woo Han <jin00woo@naver.com>
2023-08-22 20:28:51 +0900

    Created middlewares.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT;
@@ -14,14 +16,10 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((x) => x._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
